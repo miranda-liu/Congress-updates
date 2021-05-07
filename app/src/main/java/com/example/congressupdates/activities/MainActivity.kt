3be9429501f3a8f64fd55b4741ca33aabe2c5be1
MickeyLee54 <michael.jayden.lee@gmail.com>
2021-05-07 11:10:41 -0700

    This should be the Committees.kt information

--- app/src/main/java/com/example/congressupdates/activities/MainActivity.kt
+++ app/src/main/java/com/example/congressupdates/activities/MainActivity.kt
@@ -3,11 +3,33 @@ package com.example.congressupdates.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.congressupdates.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button_main_house.setOnClickListener {
+
+        }
+
+        button_main_senate.setOnClickListener {
+
+        }
+
+        button_main_committees.setOnClickListener {
+
+        }
+
+        button_main_contactInfo.setOnClickListener {
+
+        }
     }
-    // yay
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/congressupdates/models/Committees.kt
@@ -0,0 +1,37 @@
+package com.example.congressupdates.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Committees(
+    val status: String,
+    val copyright: String,
+    val congress: String,
+    val chamber: String,
+    val num_results: Int,
+    val results: List<Information>
+) : Parcelable{
+    @Parcelize
+    data class Information(
+        val id: String,
+        val name: String,
+        val chamber: String,
+        val url: String,
+        val api_url: String,
+        val chair: String,
+        val chair_id: String,
+        val chair_party: String,
+        val chair_state: String,
+        val chair_url: String,
+        val ranking_member_id: String,
+        val subcommittees: List<Subcommittees>
+    ) : Parcelable{
+        @Parcelize
+        data class Subcommittees(
+            val id: String,
+            val name: String,
+            val subcommittee_url: String
+        ) : Parcelable
+    }
+}
