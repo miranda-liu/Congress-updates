00b14a02ebb9c88d5c4cae32690f252e7b2ea3d7
miranda-liu <mirandaliu333@gmail.com>
2021-05-10 11:52:43 -0700

    Created activities for each way of searching

---
+++ app/src/main/java/com/example/congressupdates/activities/LocationSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.congressupdates.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.congressupdates.R
+
+class LocationSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_search)
+    }
+}
---
+++ app/src/main/java/com/example/congressupdates/activities/NameSearchActivity.kt
@@ -0,0 +1,13 @@
+package com.example.congressupdates.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.congressupdates.R
+
+class NameSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name_search)
+    }
+    // yay
+}
