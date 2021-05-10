0eaf0b02a87a66f1e1c90c015da5ae7aec5f68ab
miranda-liu <mirandaliu333@gmail.com>
2021-05-10 12:05:31 -0700

    Created activities for each way of searching

--- app/src/main/java/com/example/congressupdates/activities/LaunchSearchActivity.kt
+++ app/src/main/java/com/example/congressupdates/activities/LaunchSearchActivity.kt
@@ -1,12 +1,25 @@
 package com.example.congressupdates.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.congressupdates.R
+import kotlinx.android.synthetic.main.activity_launch_search.*
 
 class LaunchSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_search)
+
+        button_launchSearchActivity_locationSearch.setOnClickListener{
+            val locationSearchIntent = Intent(this, LocationSearchActivity::class.java)
+            startActivity(locationSearchIntent)
+        }
+
+        button_launchSearchActivity_nameSearch.setOnClickListener{
+            val nameSearchIntent = Intent(this, NameSearchActivity::class.java)
+            startActivity(nameSearchIntent)
+        }
     }
+
 }
