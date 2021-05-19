d132c2e0cbda2edb7a84d46e3b6644cbd30b9015
sydneymorrow <sydomrrow@gmail.com>
2021-05-19 10:17:42 -0700

    adapter class

---
+++ app/src/main/java/com/example/congressupdates/activities/CongressDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.example.congressupdates.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.congressupdates.R
+import kotlinx.android.synthetic.main.activity_congress_detail.*
+
+class CongressDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_congress_detail)
+
+        button_main_facebook.setOnClickListener {
+
+        }
+
+        button_main_firstName.setOnClickListener {
+
+        }
+
+        button_main_lastName.setOnClickListener {
+
+        }
+
+        button_main_phone.setOnClickListener {
+        }
+    }
+
+}
